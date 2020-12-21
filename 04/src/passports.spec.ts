7da65ca9c0704540346207cd4c2fb1b15e11510a
Scott Arden-Jones <s.ardenjones@gmail.com>
2020-12-21 15:10:51 +0000

    Day 4: Add complete passport field validation

--- 04/src/passports.spec.ts
+++ 04/src/passports.spec.ts
@@ -2,7 +2,6 @@ import tap from 'tap';
 
 import { parsePassport, validatePassport } from './passports';
 
-
 tap.test('parsePassport()', async t => {
   t.test('should create a well structured passport object', async t => {
     const samplePassport = `
@@ -24,21 +23,18 @@ byr:1937 iyr:2017 cid:147 hgt:183cm
 });
 
 tap.test('validatePassport()', async t => {
-  t.beforeEach((done, t) => {
-    const validPassport = parsePassport(`
+  const validPassport = parsePassport(`
 eyr:2029 ecl:blu cid:129 byr:1989
 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
-    `);
+  `);
 
-    t.context = {
-      validPassport
-    };
-
-    done();
-  })
+  const createInvalidPassport = (prop: string, val: string) => ({
+    ...validPassport,
+    [prop]: val
+  });
 
   t.test('should return true for a passport with all required fields', async t => {
-    t.ok(validatePassport(t.context.validPassport));
+    t.ok(validatePassport(validPassport));
   });
 
   t.test('should return false for a passport with missing required fields', async t => {
@@ -46,13 +42,54 @@ iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
   });
 
   t.test('should return false for a passport with a birth year that is too old', async t => {
-    const { validPassport } = t.context;
-
-    const invalidPassport = {
-      ...validPassport,
-      byr: '1919'
-    };
+    const invalidPassport = createInvalidPassport('byr', '1919');
 
     t.notOk(validatePassport(invalidPassport));
+  });
+
+  t.test('should return false for a passport with a birth year that is too recent', async t => {
+    const invalidPassport = createInvalidPassport('byr', '2003');
+
+    t.notOk(validatePassport(invalidPassport));
+  });
+
+  t.test('should return false for a passport with an invalid issue year', async t => {
+    t.notOk(validatePassport(createInvalidPassport('iyr', '2009')), 'Allows issue year from too long ago');
+    t.notOk(validatePassport(createInvalidPassport('iyr', '2021')), 'Allows issue year from the future');
+  });
+
+  t.test('should return false for a passport with an invalid expiration year', async t => {
+    t.notOk(validatePassport(createInvalidPassport('eyr', '2019')), 'Allows expiry year from the past');
+    t.notOk(validatePassport(createInvalidPassport('eyr', '2031')), 'Allows expiry year from too far in the future');
+  });
+
+  t.test('should return false for a passport with an invalid height', async t => {
+    t.notOk(validatePassport(createInvalidPassport('hgt', '200')), 'Allows height with no unit');
+  });
+
+  t.test('should return false for a passport with an invalid height in cm', async t => {
+    t.notOk(validatePassport(createInvalidPassport('hgt', '149cm')), 'Allows too short height');
+    t.notOk(validatePassport(createInvalidPassport('hgt', '194cm')), 'Allows too tall height');
+  });
+
+  t.test('should return false for a passport with an invalid height in inches', async t => {
+    t.notOk(validatePassport(createInvalidPassport('hgt', '58in')), 'Allows too short height');
+    t.notOk(validatePassport(createInvalidPassport('hgt', '77in')), 'Allows too tall height');
+  });
+
+  t.test('should return false for a passport with an invalid hair colour', async t => {
+    t.notOk(validatePassport(createInvalidPassport('hcl', 'aaa111')), 'Allows missing # symbol');
+    t.notOk(validatePassport(createInvalidPassport('hcl', '#000ggg')), 'Allows invalid characters');
+    t.notOk(validatePassport(createInvalidPassport('hcl', '#000ffff')), 'Allows too long value');
+  });
+
+  t.test('should return false for a passport with an invalid eye colour', async t => {
+    t.notOk(validatePassport(createInvalidPassport('ecl', 'blue')));
+  });
+
+  t.test('should return false for a passport with an invalid passport id', async t => {
+    t.notOk(validatePassport(createInvalidPassport('pid', '12345678')), 'Allows too short id');
+    t.notOk(validatePassport(createInvalidPassport('pid', '1234567890')), 'Allows too long id');
+    t.notOk(validatePassport(createInvalidPassport('pid', 'abc456789')), 'Allows invalid characters');
   });
 });
--- 04/src/passports.ts
+++ 04/src/passports.ts
@@ -7,25 +7,43 @@ export const parsePassport = (passportString: string) => {
   return Object.fromEntries(passportLines);
 };
 
-const validateBirthYear = (birthYearString: string) => {
-  const birthYear = parseInt(birthYearString, 10);
+const isInRange = (min: number, max: number) => (value: string) => {
+  const valueAsInt = parseInt(value, 10);
 
-  return birthYear >= 1920;
+  return valueAsInt >= min && valueAsInt <= max;
 }
 
-const T = () => true;
+const validateHeightInCm = isInRange(150, 193);
+
+const validateHeightInInches = isInRange(59, 76);
+
+const validateHeight = () => (height: string) => {
+  const result = /([0-9]{2,3})(cm|in)/.exec(height);
+
+  if(!result) return false;
+
+  const [ wholeMatch, heightValue, heightUnit ] = result;
+
+  const validate = heightUnit === 'cm' ? validateHeightInCm : validateHeightInInches;
+
+  return validate(heightValue);
+};
+
+const matches = (regEx: RegExp) => (value: string) => regEx.test(value);
+
+const isOneOf = (allowed: string[]) => (value: string) => allowed.includes(value);
 
 export const validatePassport = (passport: any) => {
   const passportKeys = Object.keys(passport);
 
   const requiredKeys: [string, (val: string) => boolean][] = [
-    ['byr', validateBirthYear],
-    ['iyr', T],
-    ['eyr', T],
-    ['hgt', T],
-    ['hcl', T],
-    ['ecl', T],
-    ['pid', T],
+    ['byr', isInRange(1920, 2002)],
+    ['iyr', isInRange(2010, 2020)],
+    ['eyr', isInRange(2020, 2030)],
+    ['hgt', validateHeight()],
+    ['hcl', matches(/^#[0-9a-f]{6}$/)],
+    ['ecl', isOneOf(['amb','blu','brn','gry','grn','hzl','oth'])],
+    ['pid', matches(/^[0-9]{9}$/)],
   ];
 
   return requiredKeys.every(([key, validator]) => {
