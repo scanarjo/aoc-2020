ab9e902634972b273a1d9aa7cf0607aed5d1cd55
Scott Arden-Jones <s.ardenjones@gmail.com>
2020-12-21 18:25:42 +0000

    Day 5: Solution for part 2

--- 05/src/index.ts
+++ 05/src/index.ts
@@ -3,12 +3,26 @@ import { promises as fs } from 'fs';
 import { parseSeatCode } from './boardingPasses';
 
 fs.readFile('./input.txt').then(file => {
-  const largestId = file
+  const seatIds = file
     .toString()
     .trim()
     .split(/[\r\n]+/)
     .map(parseSeatCode)
-    .reduce((currentMax, { id }) => Math.max(currentMax, id), 0);
+    .map(({ id }) => id)
+    .sort((a, b) => a - b);
+
+  const largestId = seatIds[seatIds.length - 1];
+
+  const candidateSeatIds = [];
+  for (let candidateId = seatIds[0]; candidateId < largestId; candidateId++) {
+    if(seatIds.find(id => id === candidateId)) continue;
+
+    if(seatIds.find(id => id === candidateId + 1) && seatIds.find(id => id === candidateId - 1)) {
+      candidateSeatIds.push(candidateId);
+    }
+  }
+
+  console.log('Your seat is one of: ', candidateSeatIds);
 
   console.log('The largest Seat ID is: ', largestId);
 })
